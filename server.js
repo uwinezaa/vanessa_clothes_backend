ef309e32fea68df998e2dea8a1f227283104a79c
uwinezaa <uwinezaa@users.noreply.github.com>
2026-04-28 19:17:56 +0200

    backend ready for render

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 const express = require('express');
+import cors from "cors";
+
+app.use(cors());
+app.use(express.json());
+const PORT = process.env.PORT || 5000;
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
